1ff4d7cea9c6311c76b44ddbd9d3436b6eaa9508
Marcos Ribeiro <131191211+Marcsfic98@users.noreply.github.com>
2025-04-09 11:16:06 -0300

    projeto finalizado e responsivo

--- js/jquery.js
+++ js/jquery.js
@@ -1,4 +1,19 @@
 $('nav a').hover(function(){
-    $('nav li').removeClass('selected');
+    $('nav.desktop li').removeClass('selected');
     $(this).parent().addClass('selected');
+})
+
+$('nav.mobile h3').click(function(){
+    $('ul.nav_mobile').slideToggle();
+})
+
+$(function(){
+    $('span a').click(function(){
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $(html , body).animate({'scrollTop':offSetTop});
+
+        return false;
+    })
 })
